23927c253485d7e6e0a27eaff17f60925ff9fc85
fedeval <federico.valenza5@gmail.com>
2022-12-26 21:23:03 +0100

    small explanatory comment

--- app/allocate.py
+++ app/allocate.py
@@ -1,5 +1,11 @@
 from .model import OrderLine, Batch
 
 def allocate(line: OrderLine, batches: list[Batch]) -> None:
+    """
+    This is a domain service since it represents a business
+    process. It could also be an object (e.g. AllocationService),
+    but it is so simple that is more expressive and readable
+    to simply define it as a function.
+    """
     batch_to_allocate = sorted(batches)[0]
     batch_to_allocate.allocate(line)
