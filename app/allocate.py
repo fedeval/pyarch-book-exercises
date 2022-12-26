ce3c14ade973504456b7883a167943e0287e62e2
fedeval <federico.valenza5@gmail.com>
2022-12-26 21:20:12 +0100

    remove print

--- app/allocate.py
+++ app/allocate.py
@@ -1,6 +1,5 @@
 from .model import OrderLine, Batch
 
 def allocate(line: OrderLine, batches: list[Batch]) -> None:
-    print([b.reference for b in sorted(batches)])
     batch_to_allocate = sorted(batches)[0]
     batch_to_allocate.allocate(line)
